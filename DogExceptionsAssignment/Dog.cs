8adf6d7fa5c29b4fe0d638abf009d7b98bc6fc94
catalinacris <catalinacucos14@yahoo.com>
2022-01-12 06:48:18 +0100

    Add project files.

---
+++ DogExceptionsAssignment/Dog.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DogExceptionsAssignment
+{
+    public class Dog
+    {
+
+        private string name; //throw ApplicationException for name length lower than 2
+        private int age; //throw ArgumentException for negative age 
+                         //throw ApplicationException for age greater than 100
+        
+
+        public Dog(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    this.name = value;
+                }
+                else //name length lower than 2
+                {
+                    throw new ApplicationException("The NAME should have at least 2 characters");
+                }
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+
+                if (value >= 0 && value <= 100)
+                {
+                    this.age = value;
+                }
+                else if (value < 0)
+                {
+                    throw new ArgumentException("The AGE should not be less then 0");
+                }
+                else // age greater than 100
+                {
+                    throw new ApplicationException("The AGE should not be greater then 100");
+                }
+
+            }
+        }
+
+        
+    }
+}
---
+++ DogExceptionsAssignment/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DogExceptionsAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            try
+            {
+                Dog dog = new Dog("Lola", 5);
+                dog.Name = "a";
+                Console.WriteLine(dog.Name);
+
+            }
+            catch (ApplicationException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+
+            try
+            {
+                Dog dog = new Dog("Lola", 5);
+                dog.Age = -3;
+                Console.WriteLine(dog.Age);
+
+            }
+            catch (ArgumentException aen)
+            {
+                Console.WriteLine(aen.Message);
+            }
+
+            try
+            {
+                Dog dog = new Dog("Lola", 5);
+                dog.Age = 111;
+                Console.WriteLine(dog.Age);
+            }
+            catch (ApplicationException fe)
+            {
+                Console.WriteLine(fe.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("Thank you!");
+            }
+
+            
+
+
+
+
+
+
+        }
+    }
+}
